609c6dc6baf6e0b764519b0e52f795e32881c731
rk14061995 <rahulkumar14061995@gmail.com>
2020-04-06 22:26:28 +0530

    Updated

--- application/views/pages/checkout.php
+++ application/views/pages/checkout.php
@@ -1,4 +1,9 @@
+<?php
+    if($this->session->userdata('logged_user')){
+       print_r($session=unserialize($this->session->logged_user))    ;
+    }
 
+?>
 
     <!-- Start All Title Box -->
     <div class="all-title-box">
@@ -27,38 +32,38 @@
                         </div>
                         <form class="needs-validation" novalidate>
                             <div class="row">
-                                <div class="col-md-6 mb-3">
+                                <!-- <div class="col-md-6 mb-3">
                                     <label for="firstName">First name *</label>
                                     <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
                                     <div class="invalid-feedback"> Valid first name is required. </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="lastName">Last name *</label>
-                                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                                </div> -->
+                                <div class="col-md-12 mb-3">
+                                    <label for="lastName">Full Name *</label>
+                                    <input type="text" class="form-control" id="lastName" placeholder="" value="<?=$session[0]->name?>" required>
                                     <div class="invalid-feedback"> Valid last name is required. </div>
                                 </div>
                             </div>
                             <div class="mb-3">
-                                <label for="username">Username *</label>
+                                <label for="username">Mobile *</label>
                                 <div class="input-group">
-                                    <input type="text" class="form-control" id="username" placeholder="" required>
+                                    <input type="text" class="form-control" id="mobile" value="<?=$session[0]->phone?>" placeholder="" readonly>
                                     <div class="invalid-feedback" style="width: 100%;"> Your username is required. </div>
                                 </div>
                             </div>
                             <div class="mb-3">
                                 <label for="email">Email Address *</label>
-                                <input type="email" class="form-control" id="email" placeholder="">
+                                <input type="email" class="form-control" id="email" value="<?=$session[0]->email?>" placeholder="">
                                 <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
                             </div>
                             <div class="mb-3">
                                 <label for="address">Address *</label>
-                                <input type="text" class="form-control" id="address" placeholder="" required>
+                                <input type="text" class="form-control" id="address" value="<?=$session[0]->address?>" placeholder="" required>
                                 <div class="invalid-feedback"> Please enter your shipping address. </div>
                             </div>
-                            <div class="mb-3">
+                            <!-- <div class="mb-3">
                                 <label for="address2">Address 2 *</label>
-                                <input type="text" class="form-control" id="address2" placeholder=""> </div>
-                            <div class="row">
+                                <input type="text" class="form-control" id="address2" placeholder=""> </div> -->
+                           <!--  <div class="row">
                                 <div class="col-md-5 mb-3">
                                     <label for="country">Country *</label>
                                     <select class="wide w-100" id="country">
@@ -81,11 +86,11 @@
                                     <div class="invalid-feedback"> Zip code required. </div>
                                 </div>
                             </div>
-                            <hr class="mb-4">
-                            <div class="custom-control custom-checkbox">
+                            <hr class="mb-4"> -->
+                            <!-- <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="same-address">
                                 <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-                            </div>
+                            </div> -->
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="save-info">
                                 <label class="custom-control-label" for="save-info">Save this information for next time</label>
@@ -143,26 +148,7 @@
                 </div>
                 <div class="col-sm-6 col-lg-6 mb-3">
                     <div class="row">
-                        <div class="col-md-12 col-lg-12">
-                            <div class="shipping-method-box">
-                                <div class="title-left">
-                                    <h3>Shipping Method</h3>
-                                </div>
-                                <div class="mb-4">
-                                    <div class="custom-control custom-radio">
-                                        <input id="shippingOption1" name="shipping-option" class="custom-control-input" checked="checked" type="radio">
-                                        <label class="custom-control-label" for="shippingOption1">Standard Delivery</label> <span class="float-right font-weight-bold">FREE</span> </div>
-                                    <div class="ml-4 mb-2 small">(3-7 business days)</div>
-                                    <div class="custom-control custom-radio">
-                                        <input id="shippingOption2" name="shipping-option" class="custom-control-input" type="radio">
-                                        <label class="custom-control-label" for="shippingOption2">Express Delivery</label> <span class="float-right font-weight-bold"><?=$webDetail->currency_?> 10.00</span> </div>
-                                    <!-- <div class="ml-4 mb-2 small">(2-4 business days)</div>
-                                    <div class="custom-control custom-radio">
-                                        <input id="shippingOption3" name="shipping-option" class="custom-control-input" type="radio">
-                                        <label class="custom-control-label" for="shippingOption3">Next Business day</label> <span class="float-right font-weight-bold">$20.00</span> </div> -->
-                                </div>
-                            </div>
-                        </div>
+                       
                         <div class="col-md-12 col-lg-12">
                             <div class="odr-box">
                                 <div class="title-left">
