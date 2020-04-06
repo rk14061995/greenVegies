8031b38c71e64d6abfcd74abb6ae418f77423624
rk14061995 <rahulkumar14061995@gmail.com>
2020-04-06 23:29:34 +0530

    Updated

--- application/views/pages/shop-detail.php
+++ application/views/pages/shop-detail.php
@@ -26,13 +26,13 @@
                             <div class="carousel-item"> <img class="d-block w-100" src="images/big-img-03.jpg" alt="Third slide"> </div> -->
                         </div>
                         <a class="carousel-control-prev" href="#carousel-example-1" role="button" data-slide="prev"> 
-						<i class="fa fa-angle-left" aria-hidden="true"></i>
-						<span class="sr-only">Previous</span> 
-					</a>
+                        <i class="fa fa-angle-left" aria-hidden="true"></i>
+                        <span class="sr-only">Previous</span> 
+                    </a>
                         <a class="carousel-control-next" href="#carousel-example-1" role="button" data-slide="next"> 
-						<i class="fa fa-angle-right" aria-hidden="true"></i> 
-						<span class="sr-only">Next</span> 
-					</a>
+                        <i class="fa fa-angle-right" aria-hidden="true"></i> 
+                        <span class="sr-only">Next</span> 
+                    </a>
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-example-1" data-slide-to="0" class="active">
                                 <img class="d-block w-100 img-fluid" src="images/smp-img-01.jpg" alt="" />
@@ -101,7 +101,7 @@
                     
                 </div>
             </div>
-			 <script type="text/javascript">
+             <script type="text/javascript">
                 $(document).ready(function(){
                     var cartUrl="<?=base_url('Cart/addToCart/')?>";
                     $('.cartt').on('click',function(){
@@ -123,36 +123,59 @@
                     });
                 });
             </script>
-			<div class="row my-5">
-				<div class="card card-outline-secondary my-4">
-					<div class="card-header">
-						<h2>Product Reviews</h2>
-					</div>
-					<div class="card-body">
-                        <?php foreach ($Reviews as $key => $value): ?>
-                           
-    						<div class="media mb-3">
-    							<div class="mr-2"> 
-    								<img class="rounded-circle border p-1" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2264%22%20height%3D%2264%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2064%2064%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_160c142c97c%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_160c142c97c%22%3E%3Crect%20width%3D%2264%22%20height%3D%2264%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2213.5546875%22%20y%3D%2236.5%22%3E64x64%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Generic placeholder image">
-    							</div>
-    							<div class="media-body">
-    								<p><?=$value->review?></p>
-    								<small class="text-muted">Posted by Anonymous on <?=$value->posted_on?></small>
-    							</div>
-    						</div>
-    						<hr>
-                        <?php endforeach; ?>
-						
-						<a href="#" class="btn hvr-hover">Leave a Review</a>
-					</div>
-				  </div>
-			</div>
+            <div class="row my-5">
+                <div class="col-md-12">
+                    <form id="contactForm_">
+                        <div class="row">
+                            
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="message" placeholder="Your Review" rows="4" data-error="Write your message" name="message" required></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="submit-button text-center">
+                                    <button class="btn hvr-hover" id="submit" type="submit">Send Review</button>
+                                    <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row my-5">
+                <div class="col-md-12">
+                    <div class="card card-outline-secondary my-4">
+                        <div class="card-header">
+                            <h2>Product Reviews</h2>
+                        </div>
+                        <div class="card-body">
+                            <?php foreach ($Reviews as $key => $value): ?>
+                               
+                                <div class="media mb-3">
+                                    <div class="mr-2"> 
+                                        <img class="rounded-circle border p-1" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2264%22%20height%3D%2264%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2064%2064%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_160c142c97c%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_160c142c97c%22%3E%3Crect%20width%3D%2264%22%20height%3D%2264%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2213.5546875%22%20y%3D%2236.5%22%3E64x64%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Generic placeholder image">
+                                    </div>
+                                    <div class="media-body">
+                                        <p><?=$value->review?></p>
+                                        <small class="text-muted">Posted by Anonymous on <?=$value->posted_on?></small>
+                                    </div>
+                                </div>
+                                <hr>
+                            <?php endforeach; ?>
+                            
+                            <a href="#" class="btn hvr-hover">Leave a Review</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
 
             <div class="row my-5">
                 <div class="col-lg-12">
                     <div class="title-all text-center">
                         <h1>Featured Products</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet lacus enim.</p>
+                        <p><?=$webDetail->tag_line?></p>
                     </div>
                     <div class="featured-products-box owl-carousel owl-theme">
                         <?php foreach ($gallery_ as $value) : ?>
